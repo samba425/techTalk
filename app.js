1e3f5f04122b0526a78404b2b7e4faed3af0b351
samba325 <asiva325@gmail.com>
2017-11-27 22:51:55 +0530

    auth added google

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+
+require('./config/passport')(passport);
+
+//routes
+const auth = require('./routes/auth');
 
 const app = express();
 
 app.get('/',(req,res) => {
     res.send("helo ....")
 })
+
+// Routes 
+
+app.use('/auth',auth);
+
 const port = process.env.PORT || 5000; 
 app.listen(port,()=> {
     console.log(`server started on port ${port}`);
---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+    mongoURI:'mongodb://samba425:samba425@ds121896.mlab.com:21896/techtalk',
+    googleClientID: '712980895605-7kcgv29o1c3jtdufc0n6q6beg9906ood.apps.googleusercontent.com',
+    googleClientSecret:'CSXrRgh-xCOLwusL7BRExWvD'
+}
